29e05aa31935c09a6f86573dfc695f0e835da54d
Alexsandro Oliveira <alexsandro.sfo@gmail.com>
2020-05-22 13:48:17 -0300

    REST testado

---
+++ src/main/java/com/alexsandro/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,20 @@
+package com.alexsandro.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String buscar() {
+		
+		return "REST funcionando";
+	}
+		
+	
+	
+}
