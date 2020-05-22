7607231a6b1c54cf416e21f86837c23a5f69abbd
Alexsandro Oliveira <alexsandro.sfo@gmail.com>
2020-05-22 14:32:11 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/alexsandro/cursomc/domain/Categoria.java
+++ src/main/java/com/alexsandro/cursomc/domain/Categoria.java
@@ -2,13 +2,22 @@ package com.alexsandro.cursomc.domain;
 
 import java.io.Serializable;
 
-public class Categoria implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id //pk
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //define geracao automatica dos ids
 	private Integer id;
 	private String nome;
-	
-	public Categoria() {}
+
+	public Categoria() {
+	}
 
 	public Categoria(Integer id, String nome) {
 		super();
@@ -56,7 +65,5 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 
 }
