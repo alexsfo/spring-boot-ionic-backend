cd37b2dabb9fe7a469361ba5bb9af6fd6496b1aa
Alexsandro Oliveira <alexsandro.sfo@gmail.com>
2020-05-31 17:28:39 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/alexsandro/cursomc/services/CategoriaService.java
+++ src/main/java/com/alexsandro/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria find(Integer id) {
+	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
