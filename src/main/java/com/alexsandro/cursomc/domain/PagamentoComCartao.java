becebd58b5b0e1d10586d804b1fb4c87222672fc
Alexsandro Oliveira <alexsandro.sfo@gmail.com>
2020-06-03 15:09:51 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/alexsandro/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/alexsandro/cursomc/domain/PagamentoComCartao.java
@@ -3,33 +3,27 @@ package com.alexsandro.cursomc.domain;
 import javax.persistence.Entity;
 
 import com.alexsandro.cursomc.domain.enums.EstadoPagamento;
+
 @Entity
-public class PagamentoComCartao extends Pagamento{
+public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
 	private Integer numeroDeParcelas;
-	
-	
-	public PagamentoComCartao() {}
 
+	public PagamentoComCartao() {
+	}
 
 	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
 		super(id, estado, pedido);
 		this.numeroDeParcelas = numeroDeParcelas;
 	}
 
-
 	public Integer getNumeroDeParcelas() {
 		return numeroDeParcelas;
 	}
 
-
 	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
 		this.numeroDeParcelas = numeroDeParcelas;
 	}
-	
-	
-	
-	
-	
+
 }
--- src/main/java/com/alexsandro/cursomc/domain/Produto.java
+++ src/main/java/com/alexsandro/cursomc/domain/Produto.java
@@ -15,7 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -28,16 +27,12 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
-	@JoinTable(
-			name = "PRODUTO_CATEGORIA", 
-			joinColumns = @JoinColumn(name = "produto_id"), 
-			inverseJoinColumns = @JoinColumn(name = "categoria_id"))
-	
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+
 	private List<Categoria> categorias = new ArrayList<>();
 
-	
 	@JsonIgnore
 	@OneToMany(mappedBy = "id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
@@ -51,12 +46,11 @@ public class Produto implements Serializable {
 		this.nome = nome;
 		this.preco = preco;
 	}
-	
-	
+
 	@JsonIgnore
-	public List<Pedido> getPedidos(){
+	public List<Pedido> getPedidos() {
 		List<Pedido> lista = new ArrayList<>();
-		for (ItemPedido x: itens) {
+		for (ItemPedido x : itens) {
 			lista.add(x.getPedido());
 		}
 		return lista;
