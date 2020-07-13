b17ddc7078300970e18c35bf0b3ab20b16d3eeb6
Alexsandro Oliveira <alexsandro.sfo@gmail.com>
2020-07-13 15:47:36 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/alexsandro/cursomc/services/ClienteService.java
+++ src/main/java/com/alexsandro/cursomc/services/ClienteService.java
@@ -10,9 +10,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.alexsandro.cursomc.domain.Cidade;
 import com.alexsandro.cursomc.domain.Cliente;
+import com.alexsandro.cursomc.domain.Endereco;
+import com.alexsandro.cursomc.domain.enums.TipoCliente;
 import com.alexsandro.cursomc.dto.ClienteDTO;
+import com.alexsandro.cursomc.dto.ClienteNewDTO;
+import com.alexsandro.cursomc.repositories.CidadeRepository;
 import com.alexsandro.cursomc.repositories.ClienteRepository;
+import com.alexsandro.cursomc.repositories.EnderecoRepository;
 import com.alexsandro.cursomc.services.exceptions.DataIntegrityException;
 import com.alexsandro.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,16 +28,32 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
-	
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null); // caso o id esteja diferente de nulo e ja exista um registro na base com o
+							// mesmo id, a operacao entendera que eh um update ao inves de um insert
+		obj =  repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		
+		return obj;
+		
+	}
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
-		return repo.save(newObj); 
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -43,25 +65,40 @@ public class ClienteService {
 			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
 		}
 	}
-	
-	public List<Cliente> findAll(){
-		return repo.findAll(); 
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
 	}
-	
-	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
+
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
-	
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Optional<Cidade> cid = cidadeRepository.findById(objDto.getCidadeId());
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid.orElseThrow());
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
-		
+
 	}
 
 }
