7fcd67d64bede71125d629e1be1328b3ab558bff
Alexsandro Oliveira <alexsandro.sfo@gmail.com>
2020-07-23 15:59:32 -0300

    Email HTML

--- src/main/java/com/alexsandro/cursomc/services/EmailService.java
+++ src/main/java/com/alexsandro/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.alexsandro.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.alexsandro.cursomc.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 
 }
--- src/main/java/com/alexsandro/cursomc/services/MockEmailService.java
+++ src/main/java/com/alexsandro/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.alexsandro.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -14,6 +16,14 @@ public class MockEmailService extends AbstractEmailService {
 		LOG.info("Simulando envio de email...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+		
 	}
 
 }
